b84781dae79000c26429485940264c50bc759117
videssmart <vipmarketing@videssmart.com>
2025-05-17 14:37:34 -0600

    commit2

--- layouts/header.php
+++ layouts/header.php
@@ -3,7 +3,6 @@ $seccion = $_GET['seccion'] ?? 'inicio';
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
